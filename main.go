767fdd03d7915736bd3a473ee777e811e0840123
otiai10 <otiai10@gmail.com>
2015-11-05 10:02:24 +0900

    Adjust to terminal size

--- main.go
+++ main.go
@@ -48,11 +48,17 @@ func run(filename string, stdout, stderr io.ReadWriter) {
 
 	// TODO: zoom
 	// TODO: or auto zoom by terminal
-	ratio := 4
 	aspect := 2
+	t := getTerminal()
+	rowcount := int(t.Row)
+	// colcount := rowcount * (img.Bounds().Max.X / img.Bounds().Max.Y)
+	colcount := int(float64(rowcount) * float64(img.Bounds().Max.X) / float64(img.Bounds().Max.Y))
+	ratio := img.Bounds().Max.Y / rowcount
 
-	for row := 0; row < img.Bounds().Max.Y/ratio; row++ {
-		for col := 0; col < img.Bounds().Max.X/ratio; col++ {
+	// for row := 0; row < img.Bounds().Max.Y/ratio; row++ {
+	for row := 0; row < rowcount; row++ {
+		// for col := 0; col < img.Bounds().Max.X/ratio; col++ {
+		for col := 0; col < colcount; col++ {
 			r, g, b, a := img.At(col*ratio+2, row*ratio+2).RGBA() // FIXME: 微調整
 			for i := 0; i < aspect; i++ {
 				fmt.Fprintf(stdout, "\x1b[48;5;%sm \x1b[m", colors.GetCodeByRGBA(r, g, b, a))
