330f1c4f8f390403557bf5788a327900a01a57be
otiai10 <otiai10@gmail.com>
2015-11-10 14:24:16 +0900

    Fix

--- gat/main.go
+++ gat/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"image"
 	"io"
 	"os"
 
 	"github.com/otiai10/gat"
 
+	_ "image/gif"
+	_ "image/jpeg"
 	_ "image/png"
 )
 
@@ -19,10 +22,10 @@ var (
 )
 
 func init() {
-	flag.IntVar(&w, "w", 0, "cols")
-	flag.IntVar(&h, "h", 0, "rows")
-	flag.BoolVar(&border, "b", false, "border style")
-	flag.BoolVar(&debug, "debug", false, "debug mode")
+	flag.IntVar(&w, "w", 0, "Width of output (cols)")
+	flag.IntVar(&h, "h", 0, "Height of output (rows)")
+	flag.BoolVar(&border, "b", false, "Add border")
+	flag.BoolVar(&debug, "debug", false, "Debug mode with cell index")
 	flag.Parse()
 }
 
@@ -32,17 +35,21 @@ func main() {
 	run(filename, stdout, stderr, w, h)
 }
 
+func onerror(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Println(err)
+	os.Exit(1)
+}
+
 func run(filename string, stdout, stderr io.ReadWriter, col, row int) {
 
 	f, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
+	onerror(err)
 
 	img, _, err := image.Decode(f)
-	if err != nil {
-		panic(err)
-	}
+	onerror(err)
 
 	var client *gat.Client
 	switch {
