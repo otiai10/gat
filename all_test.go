7b1655871a25f58e4039bdefb6fbe63281fc3a82
Hiromu OCHIAI <otiai10@gmail.com>
2019-07-09 17:56:42 +0900

    Add more tests

--- all_test.go
+++ all_test.go
@@ -22,4 +22,15 @@ func TestRun(t *testing.T) {
 	b, err := ioutil.ReadAll(o)
 	Expect(t, err).ToBe(nil)
 	Expect(t, len(b)).ToBe(15)
+
+	Because(t, "gat can accept image URL via http/https", func(t *testing.T) {
+		err := run("https://raw.githubusercontent.com/otiai10/gat/master/samples/sample.png", o, e, 2, 3)
+		Expect(t, err).ToBe(nil)
+		When(t, "given URL is not valid URL nor response is not image/*", func(t *testing.T) {
+			err = run("foobaa://github.com/otiai10/gat", o, e, 2, 3)
+			Expect(t, err).Not().ToBe(nil)
+			err = run("https://github.com/otiai10/gat", o, e, 2, 3)
+			Expect(t, err).Not().ToBe(nil)
+		})
+	})
 }
