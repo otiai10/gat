2e77f3a4ff267c0b8591f6de6318c6a91f0f59c9
Hiromu OCHIAI <otiai10@gmail.com>
2022-07-09 10:46:44 +0900

    Allow multiple files and fix interaction accordingly

--- all_test.go
+++ all_test.go
@@ -17,7 +17,7 @@ func TestMain(m *testing.M) {
 
 func TestRun(t *testing.T) {
 	o, e := bytes.NewBuffer(nil), bytes.NewBuffer(nil)
-	run("./samples/red.png", o, e, 2, 3)
+	run([]string{"./samples/red.png"}, o, e, 2, 3)
 
 	b, err := ioutil.ReadAll(o)
 	Expect(t, err).ToBe(nil)
@@ -25,12 +25,12 @@ func TestRun(t *testing.T) {
 
 	Because(t, "gat can accept image URL via http/https", func(t *testing.T) {
 		t.SkipNow()
-		err := run("https://raw.githubusercontent.com/otiai10/gat/master/samples/sample.png", o, e, 2, 3)
+		err := run([]string{"https://raw.githubusercontent.com/otiai10/gat/master/samples/sample.png"}, o, e, 2, 3)
 		Expect(t, err).ToBe(nil)
 		When(t, "given URL is not valid URL nor response is not image/*", func(t *testing.T) {
-			err = run("foobaa://github.com/otiai10/gat", o, e, 2, 3)
+			err = run([]string{"foobaa://github.com/otiai10/gat"}, o, e, 2, 3)
 			Expect(t, err).Not().ToBe(nil)
-			err = run("https://github.com/otiai10/gat", o, e, 2, 3)
+			err = run([]string{"https://github.com/otiai10/gat"}, o, e, 2, 3)
 			Expect(t, err).Not().ToBe(nil)
 		})
 	})
